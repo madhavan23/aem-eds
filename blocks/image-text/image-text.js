ddfef80aa98322dc0d66b28269d7b8a9e3d30a1d
Madhavan R <madhavan.r@mindsprint.org>
2025-04-29 15:48:27 +0530

    new fix

--- blocks/image-text/image-text.js
+++ blocks/image-text/image-text.js
@@ -5,23 +5,22 @@ export default function decorate() {
     // Find the div that contains the picture element
     const pictureElement = imageText.querySelector('div > picture');
     const imageDiv = pictureElement?.closest('div');
-  
+
     if (imageDiv) {
       imageDiv.classList.add('picture-container');
-  
+
       const parentDiv = imageDiv.parentElement;
       if (parentDiv) {
         parentDiv.classList.add('image-text-container');
       }
     }
-  
+
     const textContainer = document.createElement('div');
     textContainer.classList.add('text-wrapper');
-  
+
     const detailsDivs = imageText.querySelectorAll(
-      'div[data-aue-prop="title"], div[data-aue-prop="description"], p.button-container'
-    );
-  
+      'div[data-aue-prop="title"], div[data-aue-prop="description"], p.button-container',);
+
     detailsDivs.forEach((div) => {
       const parentDiv = div.parentElement;
       if (parentDiv && parentDiv.parentElement === imageText) {
@@ -30,14 +29,14 @@ export default function decorate() {
         textContainer.appendChild(div);
       }
     });
-  
+
     // Remove empty divs inside the wrapper
     imageText.querySelectorAll('div').forEach((div) => {
       if (div.childNodes.length === 0) {
         div.remove();
       }
     });
-  
+
     // Insert the text container after the image container
     if (imageDiv && imageDiv.parentNode) {
       imageDiv.parentNode.insertBefore(textContainer, imageDiv.nextSibling);
@@ -46,4 +45,3 @@ export default function decorate() {
     }
   });
 }
-  
