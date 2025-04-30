efeff29f1bbe9e53e2e50ef8103d7d2002ad4623
Madhavan R <madhavan.r@mindsprint.org>
2025-04-30 16:12:58 +0530

    new fix

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -5,7 +5,7 @@ export default function decorate() {
     const textContainer = document.createElement('div');
     textContainer.classList.add('text-wrapper');
 
-    const content = item.querySelectorAll('div[data-aue-prop="title"]', 'div[data-aue-prop="description"]',);
+    const content = item.querySelectorAll('div[data-aue-prop="title"]', 'div[data-aue-prop="description"]');
     content.forEach((div) => {
       const parentDiv = div.parentElement;
       if (parentDiv && parentDiv.parentElement === item) {
