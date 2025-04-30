306d0c1a3381038c522e815eaaa40dc39cbf8c43
Madhavan R <madhavan.r@mindsprint.org>
2025-04-30 16:04:58 +0530

    eslint errro fix

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,15 +1,20 @@
-export default function decorate(block){
-    const videoWrappers = document.querySelectorAll('.video-wrapper');
+export default function decorate() {
+  const videoWrappers = document.querySelectorAll('.video-wrapper');
 
-    videoWrappers.forEach((item) => {
-        const textContainer= document.createElement('div');
-        textContainer.classList.add('text-wrapper');
+  videoWrappers.forEach((item) => {
+      const textContainer = document.createElement('div');
+      textContainer.classList.add('text-wrapper');
 
-        const content = item.querySelectorAll(
-            'div[data-aue-prop="title"]','div[data-aue-prop="description"]',
-        );
-        
-    
-    
-    });
+      const content = item.querySelectorAll(
+          'div[data-aue-prop="title"]','div[data-aue-prop="description"]', 
+      );
+      content.forEach((div) => {
+        const parentDiv = div.parentElement;
+        if (parentDiv && parentDiv.parentElement === item) {
+          textContainer.appendChild(parentDiv);
+        } else {
+          textContainer.appendChild(div);
+        }
+      });;
+  });
 }
