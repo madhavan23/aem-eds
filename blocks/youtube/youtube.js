c1ed21f9806f6b1d80b2215a1c480d123125bb9c
Madhavan R <madhavan.r@mindsprint.org>
2025-05-13 13:03:41 +0530

    error fix

--- blocks/youtube/youtube.js
+++ blocks/youtube/youtube.js
@@ -2,22 +2,22 @@ export default function decorate() {
   // Select all youtube blocks
   const youtubeBlocks=document.querySelectorAll('.youtube-wrapper');
   youtubeBlocks.forEach((youtubeBlock) => {
-    //videoID
-    const videoID=youtubeBlock.querySelector('div[data-aue-prop="videoid"]');
+    // videoID
+    const videoID = youtubeBlock.querySelector('div[data-aue-prop="videoid"]');
     const videoContainer = document.createElement('div');
     videoContainer.classList.add('video-container');
-    
-    if(videoID) {
-        videoContainer.appendChild.add(videoID.cloneNode(true));
+
+    if (videoID) {
+      videoContainer.appendChild.add(videoID.cloneNode(true));
     }
-    
+  
     // Create text container for title and description
     const textContainer = document.createElement('div');
     textContainer.classList.add('text-container');
 
     // Select title and description elements
-    const titleDiv = videoBlock.querySelector('div[data-aue-prop="title"]');
-    const descriptionDiv = videoBlock.querySelector('div[data-aue-prop="description"]');
+    const titleDiv = youtubeBlock.querySelector('div[data-aue-prop="title"]');
+    const descriptionDiv = youtubeBlock.querySelector('div[data-aue-prop="description"]');
 
     if (titleDiv) {
       // Append a clone or move the element to avoid removing from original place
