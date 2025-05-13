900307f38cfaa5b5124f8fbc1d7a37a2b0b91d2e
Madhavan R <madhavan.r@mindsprint.org>
2025-05-13 12:54:52 +0530

    js

---
+++ blocks/youtube/youtube.js
@@ -0,0 +1,30 @@
+export default function decorate() {
+  // Select all youtube blocks
+  const youtubeBlocks=document.querySelectorAll('.youtube-wrapper');
+  youtubeBlocks.forEach((youtubeBlock) => {
+    //videoID
+    const videoID=youtubeBlock.querySelector('div[data-aue-prop="videoid"]');
+    const videoContainer = document.createElement('div');
+    videoContainer.classList.add('video-container');
+    
+    if(videoID) {
+        videoContainer.appendChild.add(videoID.cloneNode(true));
+    }
+    
+    // Create text container for title and description
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+
+    // Select title and description elements
+    const titleDiv = videoBlock.querySelector('div[data-aue-prop="title"]');
+    const descriptionDiv = videoBlock.querySelector('div[data-aue-prop="description"]');
+
+    if (titleDiv) {
+      // Append a clone or move the element to avoid removing from original place
+      textContainer.appendChild(titleDiv.cloneNode(true));
+    }
+    if (descriptionDiv) {
+      textContainer.appendChild(descriptionDiv.cloneNode(true));
+    }
+  });
+}
