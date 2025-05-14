476ee708a0994e6a7a38fa298b171952dc2c44a1
Madhavan R <madhavan.r@mindsprint.org>
2025-05-14 11:21:42 +0530

    remove js

--- blocks/video/video.js
+++
@@ -1,7 +0,0 @@
-// export default function decorate() {
-//   // Select all video blocks
-//   const autoVideoBlocks = document.querySelectorAll('.video-wrapper');
-//   autoVideoBlocks.forEach((autoVideoBlock) => {
-    
-//   });
-// }
