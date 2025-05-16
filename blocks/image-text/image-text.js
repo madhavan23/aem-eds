148052513f36f8c0b36c405681bbcbc4037ba7b2
Madhavan R <madhavan.r@mindsprint.org>
2025-05-16 10:01:09 +0530

    remove js

--- blocks/image-text/image-text.js
+++ blocks/image-text/image-text.js
@@ -1,48 +1,48 @@
-export default function decorate() {
-  // Select all elements
-  const imageTextWrapper = document.querySelectorAll('.image-text-wrapper');
-  imageTextWrapper.forEach((imageText) => {
-    // Find the div that contains the picture element
-    const pictureElement = imageText.querySelector('div > picture');
-    const imageDiv = pictureElement?.closest('div');
+// export default function decorate() {
+//   // Select all elements
+//   const imageTextWrapper = document.querySelectorAll('.image-text-wrapper');
+//   imageTextWrapper.forEach((imageText) => {
+//     // Find the div that contains the picture element
+//     const pictureElement = imageText.querySelector('div > picture');
+//     const imageDiv = pictureElement?.closest('div');
 
-    if (imageDiv) {
-      imageDiv.classList.add('picture-container');
+//     if (imageDiv) {
+//       imageDiv.classList.add('picture-container');
 
-      const parentDiv = imageDiv.parentElement;
-      if (parentDiv) {
-        parentDiv.classList.add('image-text-parent-container');
-      }
-    }
+//       const parentDiv = imageDiv.parentElement;
+//       if (parentDiv) {
+//         parentDiv.classList.add('image-text-parent-container');
+//       }
+//     }
 
-    const textContainer = document.createElement('div');
-    textContainer.classList.add('text-wrapper');
+//     const textContainer = document.createElement('div');
+//     textContainer.classList.add('text-wrapper');
 
-    const detailsDivs = imageText.querySelectorAll(
-      'div[data-aue-prop="title"], div[data-aue-prop="description"], p.button-container',
-    );
+//     const detailsDivs = imageText.querySelectorAll(
+//       'div[data-aue-prop="title"], div[data-aue-prop="description"], p.button-container',
+//     );
 
-    detailsDivs.forEach((div) => {
-      const parentDiv = div.parentElement;
-      if (parentDiv && parentDiv.parentElement === imageText) {
-        textContainer.appendChild(parentDiv);
-      } else {
-        textContainer.appendChild(div);
-      }
-    });
+//     detailsDivs.forEach((div) => {
+//       const parentDiv = div.parentElement;
+//       if (parentDiv && parentDiv.parentElement === imageText) {
+//         textContainer.appendChild(parentDiv);
+//       } else {
+//         textContainer.appendChild(div);
+//       }
+//     });
 
-    // Remove empty divs inside the wrapper
-    imageText.querySelectorAll('div').forEach((div) => {
-      if (div.childNodes.length === 0) {
-        div.remove();
-      }
-    });
+//     // Remove empty divs inside the wrapper
+//     imageText.querySelectorAll('div').forEach((div) => {
+//       if (div.childNodes.length === 0) {
+//         div.remove();
+//       }
+//     });
 
-    // Insert the text container after the image container
-    if (imageDiv && imageDiv.parentNode) {
-      imageDiv.parentNode.insertBefore(textContainer, imageDiv.nextSibling);
-    } else {
-      imageText.appendChild(textContainer);
-    }
-  });
-}
+//     // Insert the text container after the image container
+//     if (imageDiv && imageDiv.parentNode) {
+//       imageDiv.parentNode.insertBefore(textContainer, imageDiv.nextSibling);
+//     } else {
+//       imageText.appendChild(textContainer);
+//     }
+//   });
+// }
